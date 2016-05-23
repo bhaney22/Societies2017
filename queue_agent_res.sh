bce19c12e5092f01ce64c17f86e6e65f7b0c1da0
jgsher@clemson.edu <jgsherw@clemson.edu>
2016-05-23 13:36:14 -0400

    Finished configuring agent res pbs.

---
+++ queue_agent_res.sh
@@ -0,0 +1,20 @@
+#Runs everything. 30 runs each of all 1,3,5,7 r/t , with and without stock, with and without DevDevs. Should be between 6,000 and 9,000 computing hours.
+
+cd PBS_scripts/agentResVariations
+
+for i in {1..30}
+do
+	echo "Queueing run #$i"
+	qsub NoDev_5_12_12_col.pbs
+	qsub NoDev_5_12_12_nor.pbs
+	qsub NoDev_5_12_24_col.pbs
+	qsub NoDev_5_12_24_nor.pbs
+	qsub NoDev_5_24_12_col.pbs
+	qsub NoDev_5_24_12_nor.pbs
+	qsub NoDev_5_24_48_col.pbs
+	qsub NoDev_5_24_48_nor.pbs
+	qsub NoDev_5_48_24_col.pbs
+	qsub NoDev_5_48_24_nor.pbs
+	qsub NoDev_5_48_48_col.pbs
+	qsub NoDev_5_48_48_nor.pbs
+done
